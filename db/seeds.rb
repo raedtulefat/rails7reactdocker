cfeaf59c9bb655ef20d7314f84634db12f29e193
raedtulefat <raedtulefat@theurbanwriters.com>
2022-02-17 20:03:07 -0500

    initial commit

---
+++ db/seeds.rb
@@ -0,0 +1,16 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# User.create(username: "user", password: "password")
+
+Comment.create(
+  subject: "Comment subject 1",
+  body: "Comment body 1")
+
+Comment.create(
+  subject: "Comment subject 2",
+  body: "Comment body 2")
+
+Comment.create(
+  subject: "Comment subject 3",
+  body: "Comment body 3")
